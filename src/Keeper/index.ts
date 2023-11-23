0d0f04ead221ca5ba2711890c9156b148fdd1db2
Ajand Mardalizad <aseedgeek@gmail.com>
2023-11-23 11:27:00 +0400

    geb initialization added

--- src/Keeper/index.ts
+++ src/Keeper/index.ts
@@ -1,22 +1,31 @@
 import { ArgsParser } from "./Initializer";
-import { ethers } from "ethers";
+import { ethers, utils as ethersUtils } from "ethers";
+import { Geb, utils } from "@hai-on-op/sdk";
 import { KeyPassSplitter, createWallet } from "./Initializer/SignerFactory";
+
+interface KeeperOverrides {
+  provider?: ethers.providers.JsonRpcProvider;
+  signer?: ethers.Signer;
+}
 
 export class Keeper {
   args;
   provider: ethers.providers.JsonRpcProvider;
   signer: ethers.Signer;
+  geb: Geb;
 
-  constructor(argsList: string[]) {
+  constructor(argsList: string[], overrides: KeeperOverrides = {}) {
     this.args = ArgsParser(argsList);
-    this.provider = new ethers.providers.JsonRpcProvider(
-      this.args["--rpc-uri"]
-    );
+    this.provider = overrides.provider
+      ? overrides.provider
+      : new ethers.providers.JsonRpcProvider(this.args["--rpc-uri"]);
 
     const keyFile = KeyPassSplitter(String(this.args["--eth-key"]));
     const wallet = createWallet(keyFile).connect(this.provider);
 
     this.signer = wallet;
+
+    this.geb = new Geb("optimism-goerli", this.provider);
   }
 }
 
