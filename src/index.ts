109ccbaa6f0cae0465a3a00368ce268ff5b82d48
Ajand Mardalizad <aseedgeek@gmail.com>
2024-02-12 12:49:00 +0400

    graceful shutdown added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import { ArgsParser } from "./Keeper/Initializer";
 import Keeper from "./Keeper";
 
+import { sleep } from "./Keeper";
+
 const main = async () => {
   const args = process.argv.slice(2);
 
-  const parsedArgs = ArgsParser(args);
-
   const keeper = new Keeper(args);
+
+  const gracefulShutdown = async () => {
+    await keeper.shutdown();
+    await sleep(50);
+    keeper.transactionQueue.status$.subscribe((status) => {
+      if (status === 3) {
+        process.exit();
+      }
+    });
+
+    await sleep(2 * 60 * 1000);
+    process.exit();
+  };
+
+  process.on("SIGTERM", () => {
+    gracefulShutdown();
+  });
+  process.on("SIGINT", () => {
+    gracefulShutdown();
+  });
 };
 
 main();
