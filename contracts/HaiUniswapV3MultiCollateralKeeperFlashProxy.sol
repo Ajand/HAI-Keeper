cd8ea118f949f06d1d93b600dc6464bff929cf65
Ajand Mardalizad <ajand@Ajands-MacBook-Pro.local>
2024-05-10 16:41:21 +0400

    hai dependencies added for flash swap

---
+++ contracts/HaiUniswapV3MultiCollateralKeeperFlashProxy.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.8.19;
+
+import {ICollateralAuctionHouse} from "./interfaces/hai/ICollateralAuctionHouse.sol";
+import {ISAFEEngine} from "./interfaces/hai/ISAFEEngine.sol";
+import {ICollateralJoin} from "./interfaces/hai/utils/ICollateralJoin.sol";
+import {ICoinJoin} from "./interfaces/hai/utils/ICoinJoin.sol";
+import {ILiquidationEngine} from "./interfaces/hai/ILiquidationEngine.sol";
+
+// import IERC20
+// import IWETH
+
+contract HaiUniswapV3MultiCollateralKeeperFlashProxy {
+
+}
---
+++ test/flash-proxy/HaiUniswapV3MultiCollateralKeeperFlashProxy.ts
@@ -0,0 +1,8 @@
+
+
+const haiEthPair1PercentPool = "0x146b020399769339509c98B7B353d19130C150EC"
+
+
+// Flash Proxy Deployment
+
+// 
