7c655efd6615ff36081452475ca209b6fa2cd693
Aj Maz <ajand@Ajands-MacBook-Pro.local>
2024-05-15 19:20:32 +0400

    flash swap proxy constructor added

--- contracts/HaiUniswapV3MultiCollateralKeeperFlashProxy.sol
+++ contracts/HaiUniswapV3MultiCollateralKeeperFlashProxy.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
 import {ICollateralAuctionHouse} from "./interfaces/hai/ICollateralAuctionHouse.sol";
@@ -6,7 +7,6 @@ import {ICollateralJoin} from "./interfaces/hai/utils/ICollateralJoin.sol";
 import {ICoinJoin} from "./interfaces/hai/utils/ICoinJoin.sol";
 import {ILiquidationEngine} from "./interfaces/hai/ILiquidationEngine.sol";
 
-
 // import IERC20
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import IWETH
@@ -14,15 +14,48 @@ import {IWETH} from "./interfaces/IWETH.sol";
 
 /*
     /// @notice Constructor
-    /// @param wethAddress WETH address
-    /// @param systemCoinAddress System coin address
-    /// @param coinJoinAddress CoinJoin address
-    /// @param liquidationEngineAddress Liquidation engine address
 
  */
 
 contract HaiUniswapV3MultiCollateralKeeperFlashProxy {
-    constructor() public {
+    IWETH public weth;
+    IERC20 public systemCoin;
+    ICoinJoin public coinJoin;
+    ILiquidationEngine public liquidationEngine;
+    ISAFEEngine public safeEngine;
 
+    /// @param wethAddress WETH address
+    /// @param systemCoinAddress System coin address
+    /// @param coinJoinAddress CoinJoin address
+    /// @param liquidationEngineAddress Liquidation engine address
+    constructor(
+        address wethAddress,
+        address systemCoinAddress,
+        address coinJoinAddress,
+        address liquidationEngineAddress
+    ) {
+        if (wethAddress == address(0)) {
+            revert NullWeth();
+        }
+        if (systemCoinAddress == address(0)) {
+            revert NullSystemCoin();
+        }
+        if (coinJoinAddress == address(0)) {
+            revert NullCoinJoin();
+        }
+        if (liquidationEngineAddress == address(0)) {
+            revert NullLiquidationEngine();
+        }
+
+        weth = IWETH(wethAddress);
+        systemCoin = IERC20(systemCoinAddress);
+        coinJoin = ICoinJoin(coinJoinAddress);
+        liquidationEngine = ILiquidationEngine(liquidationEngineAddress);
+        safeEngine = ISAFEEngine(liquidationEngine.safeEngine());
     }
+
+    error NullWeth();
+    error NullSystemCoin();
+    error NullCoinJoin();
+    error NullLiquidationEngine();
 }
--- test/flash-proxy/HaiUniswapV3MultiCollateralKeeperFlashProxy.ts
+++ test/flash-proxy/HaiUniswapV3MultiCollateralKeeperFlashProxy.ts
@@ -7,17 +7,96 @@ const haiEthPair1PercentPool = "0x146b020399769339509c98B7B353d19130C150EC";
 
 //
 
+const AddressZero = hre.ethers.constants.AddressZero;
+
+const deploymentParams = {
+  weth: "0x4200000000000000000000000000000000000006",
+  systemCoin: "0x10398AbC267496E49106B07dd6BE13364D10dC71",
+  coinJoin: "0x30Ce72230A47A0967B7e52A1bAE0178DbD7c6eA3",
+  liquidationEngine: "0x8Be588895BE9B75F9a9dAee185e0c2ad89891b56",
+};
+
+const SAFEEngine = "0x9Ff826860689483181C5FAc9628fd2F70275A700";
+
 describe("Uniswap V3 Multicollateral Flash Swap Proxy", () => {
   async function deploy() {
     const FlashProxy = await hre.ethers.getContractFactory(
       "HaiUniswapV3MultiCollateralKeeperFlashProxy"
     );
-    const flashSwap = await FlashProxy.deploy();
+    const flashSwap = await FlashProxy.deploy(
+      deploymentParams.weth,
+      deploymentParams.systemCoin,
+      deploymentParams.coinJoin,
+      deploymentParams.liquidationEngine
+    );
 
     return { flashSwap };
   }
 
-  it("Should be able to deploy flash swap proxy", async () => {
-    const { flashSwap } = await deploy();
+  describe("Deployment", () => {
+    it("Must not be able to set zero address as the params of the constructor", async () => {
+      const FlashProxy = await hre.ethers.getContractFactory(
+        "HaiUniswapV3MultiCollateralKeeperFlashProxy"
+      );
+      await expect(
+        FlashProxy.deploy(
+          AddressZero,
+          deploymentParams.systemCoin,
+          deploymentParams.coinJoin,
+          deploymentParams.liquidationEngine
+        )
+      ).to.be.revertedWithCustomError(FlashProxy, "NullWeth");
+      await expect(
+        FlashProxy.deploy(
+          deploymentParams.weth,
+          AddressZero,
+          deploymentParams.coinJoin,
+          deploymentParams.liquidationEngine
+        )
+      ).to.be.revertedWithCustomError(FlashProxy, "NullSystemCoin");
+
+      await expect(
+        FlashProxy.deploy(
+          deploymentParams.weth,
+          deploymentParams.systemCoin,
+          AddressZero,
+          deploymentParams.liquidationEngine
+        )
+      ).to.be.revertedWithCustomError(FlashProxy, "NullCoinJoin");
+
+      await expect(
+        FlashProxy.deploy(
+          deploymentParams.weth,
+          deploymentParams.systemCoin,
+          deploymentParams.coinJoin,
+          AddressZero
+        )
+      ).to.be.revertedWithCustomError(FlashProxy, "NullLiquidationEngine");
+    });
+
+    it("Must be deployed properly with right params", async () => {
+      const FlashProxy = await hre.ethers.getContractFactory(
+        "HaiUniswapV3MultiCollateralKeeperFlashProxy"
+      );
+
+      const flashSwap = await FlashProxy.deploy(
+        deploymentParams.weth,
+        deploymentParams.systemCoin,
+        deploymentParams.coinJoin,
+        deploymentParams.liquidationEngine
+      );
+
+      expect(await flashSwap.weth()).to.be.equal(deploymentParams.weth);
+      expect(await flashSwap.systemCoin()).to.be.equal(
+        deploymentParams.systemCoin
+      );
+      expect(await flashSwap.coinJoin()).to.be.equal(deploymentParams.coinJoin);
+      expect(await flashSwap.liquidationEngine()).to.be.equal(
+        deploymentParams.liquidationEngine
+      );
+      expect(await flashSwap.safeEngine()).to.be.equal(SAFEEngine);
+    });
   });
+
+  
 });
